5bc8097b61054428a508e40fe52e527f03454884
Shonuma23 <48305498+Shonuma23@users.noreply.github.com>
2019-11-08 12:43:44 +0530

    Update progk-mean.py

--- progk-mean.py
+++ progk-mean.py
@@ -1,4 +1,3 @@
-# -----Kmeans----
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.cluster import KMeans
@@ -8,7 +7,7 @@ iris = datasets.load_iris()
 X_train,X_test,y_train,y_test = train_test_split(iris.data,iris.target)
 model =KMeans(n_clusters=3)
 model.fit(X_train,y_train)
-metrics.accuracy_score(y_test,model.predict(X_test))
+print(metrics.accuracy_score(y_test,model.predict(X_test)))
 
 #-------Expectation and Maximization----------
 from sklearn.mixture import GaussianMixture
@@ -16,3 +15,4 @@ model2 = GaussianMixture(n_components=3)
 model2.fit(X_train,y_train)
 model2.score
 print(metrics.accuracy_score(y_test,model2.predict(X_test)))
+
